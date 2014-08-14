0bd3112de1c2bf22d76243405b26854e1d641300
Juani Ruiz Echazu <hey@juan-i.com.ar>
2014-08-13 23:58:50 -0300

    will finish this bad boy up tomorrow. need to rest

--- js/site.js
+++ js/site.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
-	//do stuff
+	var windowSize = $(window).width();
+  //do stuff
   $("#compare-1").twentytwenty({
     before: "SVG",
     after: "PNG"
@@ -13,19 +14,40 @@ $(document).ready(function() {
     after: "PNG"
   });
 
-  var $section = $('#zoomable');
-  var $panzoom = $section.find('.panzoom-parent').panzoom({
-    $zoomIn: $section.find(".zoom-in"),
-    $zoomOut: $section.find(".zoom-out"),
-    $zoomRange: $section.find(".zoom-range"),
-    startTransform: 'scale(.75)',
-    minScale: 1,
-    maxScale: 7,
-    increment: 0.1,
-    contain: "invert"
-  });
+  if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+    $("body").addClass("mobile");
+    var $section = $('#zoomable');
+    var $panzoom = $section.find('.panzoom-parent').panzoom({
+      $zoomIn: $section.find(".zoom-in"),
+      $zoomOut: $section.find(".zoom-out"),
+      $zoomRange: $section.find(".zoom-range"),
+      startTransform: 'scale(.4)',
+      minScale: 0.4,
+      maxScale: 7,
+      increment: 0.2,
+      contain: false
+    });
+  } else {
+    $("#svg-pan").attr("width", "776");
+    var $section = $('#zoomable');
+    var $panzoom = $section.find('.panzoom-parent').panzoom({
+      $zoomIn: $section.find(".zoom-in"),
+      $zoomOut: $section.find(".zoom-out"),
+      $zoomRange: $section.find(".zoom-range"),
+      startTransform: 'scale(1)',
+      minScale: 1,
+      maxScale: 7,
+      increment: 0.1,
+      contain: "invert"
+    });
+  }
 
   $(".zoom-range").on("input change", function(){
     $section.find('.panzoom-parent').panzoom( 'zoom', {increment: .01});
   });
 });
+
+var wow = new WOW({
+  mobile: false
+});
+wow.init();
